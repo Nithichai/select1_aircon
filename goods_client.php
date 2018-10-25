8351627b9c054f32889daebecad393ca0b65054a
Nithichai Thepmong <rockon23146@gmail.com>
2018-10-25 15:08:22 +0700

    make it better

--- goods_client.php
+++ goods_client.php
@@ -3,8 +3,12 @@
     $client = new Zend\Soap\Client('https://aircon-select1-server.herokuapp.com/server.php?wsdl');
 
     header('Content-Type: text/xml');
+    $name = 'Nopy';
+    $addr = 'Namek';
+    $weight = sprintf("%.3f", 0.1);
+    $data_str = $name . ',' . $addr . ',' . $weight;
     try {
-        $result = $client->post_goods(['Nopy,Namek,0.1']);
+        $result = $client->post_goods(['data_packet' => $data_str]);
         echo $result->post_goodsResult;
     } catch (SoapFault $e) {
         echo '<?xml version="1.0" encoding="UTF-8"?>' . 
