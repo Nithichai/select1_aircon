7e53cc7adb53a08493871403e1d9c234076b38c2
Nithichai Thepmong <rockon23146@gmail.com>
2018-11-01 02:14:39 +0700

    make it better

--- goods_update_client.php
+++ goods_update_client.php
@@ -3,12 +3,18 @@
     $client = new Zend\Soap\Client('https://aircon-select1-server.herokuapp.com/server.php?wsdl');
 
     header('Content-Type: text/xml');
-    $id = '1';
-    try {
-        $result = $client->update_goods(['id' => $id]);
-        echo $result->update_goodsResult;
-    } catch (SoapFault $e) {
+    $id = $_GET['id'];
+    if (is_numeric($id)) {
+        try {
+            $result = $client->update_goods(['id' => $id]);
+            echo $result->update_goodsResult;
+        } catch (SoapFault $e) {
+            echo '<?xml version="1.0" encoding="UTF-8"?>' . 
+                '<error>' . $e->getMessage() . '</error>';
+        }
+    } else {
         echo '<?xml version="1.0" encoding="UTF-8"?>' . 
-            '<error>' . $e->getMessage() . '</error>';
+                '<error>Please search in number</error>';
     }
+    
 ?>
